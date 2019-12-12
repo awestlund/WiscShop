c335a39500b4494013f8b861d95cf8189febb182
Amber Westlund <awestlund@wisc.edu>
2019-12-12 17:19:12 -0600

    okay

--- webhook/webhook.js
+++ webhook/webhook.js
@@ -31,77 +31,124 @@ app.post('/', express.json(), (req, res) => {
   const agent = new WebhookClient({ request: req, response: res })
 
   function welcome() {
+    console.log("here");
     agent.add('Webhook works!')
   }
 
+  //navigate to the cart
   async function navCart() {
+    //navigating to your cart
 
   }
 
+  // navigate to a product poge
   async function navProducts() {
 
   }
 
+  //navigate to a category page
   async function navPages() {
+    //Navigating to new page...
 
   }
 
+  //navigate to the Home page
   async function navHome() {
+    //Navigating Home
 
   }
   
+  //navigate back
   async function back() {
 
+    //navigating back
   }
 
+  //logout the user
   async function logout() {
+    //Logging you out.
 
   }
 
+  //list out tag/filter options
   async function tags() {
+    //listing the tags
 
   }
 
+  //list out items in the cart and the cart total $$$
   async function reviewCart() {
 
+    //Here's a review of your cart:
+
   }
 
+  //get reviews for a product if it has any
   async function prodRev() {
+    //Here are the reviews:
 
   }
 
+  //get ratings for a product if it has any
   async function prodRate() {
+    //Here is the user rating:
 
   }
 
+  //list out items in the cart and the cart total $$$
   async function cartContent() {
 
+    //Listing cart items
   }
 
+  //confirm user order
   async function confirmCart() {
-
+    //cart content
+    //cart confirm
+    //Placing your order!
   }
 
+  //delete an item from the cart
   async function cartDelete() {
 
+    //Deleting the item from your cart
   }
 
+  //add item to the cart
   async function cartAdd() {
-
+    //ask for product details like quantity
   }
 
+  //list out possible item catigories to choose from
   async function categories() {
+    //Here are the categories of products:
 
   }
 
+  //get username
+  async function username(){
+    //I entered your username.
+
+  }
+
+  //get passowrd
+  async function password(){
+    //I entered the password.
+  }
+
+  //login the user
   async function login() {
     // You need to set this from `username` entity that you declare in DialogFlow
     username = null
     // You need to set this from password entity that you declare in DialogFlow
     password = null
+
+    //get and set token with login info
     await getToken()
 
     agent.add(token)
+
+    //Logging you on!
   }
 
 
@@ -124,6 +171,8 @@ app.post('/', express.json(), (req, res) => {
   intentMap.set('Cart Delete', cartDelete)
   intentMap.set('Cart Add', cartAdd)
   intentMap.set('Categories', categories)
+  intentMap.set('Username', username)
+  intentMap.set('Password', password)
 
   agent.handleRequest(intentMap)
 })
